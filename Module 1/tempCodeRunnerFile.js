82c2afa9a35224816ddca8e58702f503e82ba2d1
nazmulh0001 <nazmulhasangub001@gmail.com>
2026-08-24 18:08:19 +0600

    first commit

---
+++ Module 1/tempCodeRunnerFile.js
@@ -0,0 +1,1 @@
+console.log(fullName.toUpperCase())
---
+++ Module 1/variable.js
@@ -0,0 +1,75 @@
+// console.log("Hello js");
+
+// var firstName = "Jahanara";
+// var lastName = "Imam";
+
+// var fullName = `${firstName} ${lastName}`;
+// console.log(fullName.toUpperCase())
+
+// falsey values in js -> 0 "" false null undefined NaN
+
+// const naz = () => console.log("Hello Nazmul");
+
+// console.log(naz);
+// naz();
+
+// let naz;
+// console.log(typeof(naz));
+
+// let naz = null;
+// console.log(typeof(naz));
+
+// console.log(Number.MIN_VALUE)
+// console.log(Number.MAX_VALUE)
+
+// let message = 'I\' am nazmul hasan'
+// console.log(message)
+
+// let name = "Nazmul";
+// name[0] = "n";
+// console.log(name);
+
+// let contact = {
+//     firstName: 'John',
+//     lastName: 'Doe',
+//     email: 'john.doe@example.com',
+//     phone: '(408)-555-9999',
+//     address: {
+//         building: '4000',
+//         street: 'North 1st street',
+//         city: 'San Jose',
+//         state: 'CA',
+//         country: 'USA'
+//     }
+// }
+
+// console.log(contact.address.street)
+
+// console.log(Boolean(''));
+// console.log(typeof 123n);
+// let num = 071;
+// console.log(num);
+
+// console.log(3.14e7);
+// console.log(0.2 + 0.1);
+// console.log(typeof 9007199254740991n);
+
+// let num = 100;
+// console.log(typeof(num));
+
+// let mesage = `"I'm good". She said`;
+// console.log(mesage)
+
+// let className = "btn ";
+// className += "btn-primary ";
+// className += "none";
+// console.log(className);
+
+// let person = {
+//     firstName : "Nazmul",
+//     lastName : "Hasan"
+// }
+
+// person.age = 20;
+
+// console.log(person.age)
