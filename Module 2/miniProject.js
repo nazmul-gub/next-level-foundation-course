6b0e74b1789866d97597e3e26be9a537c7d57fc4
nazmulh0001 <nazmulhasangub001@gmail.com>
2026-08-25 22:13:50 +0600

    created a miniProject student portal

---
+++ Module 2/miniProject.js
@@ -0,0 +1,47 @@
+//mini project
+let students = [
+    { id: 101, name: "Rahim", age: 20, department: "CSE" },
+    { id: 102, name: "Karim", age: 22, department: "EEE" },
+];
+
+function addStudent(name, age, dept) {
+    let newId = students.length > 0 ? students[students.length - 1].id + 1 : 101;
+    let newStudent = {
+        id : newId,
+        name : name,
+        age : age,
+        department : dept,
+    }
+
+    students.push(newStudent);
+    console.log("Student added successfully!");
+}
+
+addStudent("Jahan", 21, "AI");
+
+function getAll() {
+    for(const student of students) {
+        console.log(student);
+    }
+}
+
+function findStudent(id) {
+    let foundStdnt = null;
+
+    for(const i of students) {
+        if(i.id == id) {
+            foundStdnt = i; break;
+        }
+    }
+
+    if(foundStdnt != null) {
+        console.log("Student found successfully!");
+    } else {
+        console.log("Student not exists")
+    }
+}
+
+
+function deleteStudent(id) {
+    
+}
