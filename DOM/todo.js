cdd2dd3200388848d3eaeb13d6b2ae4be4c63012
nazmulh0001 <nazmulhasangub001@gmail.com>
2026-08-29 17:39:29 +0600

    learning dom in js

---
+++ DOM/todo.js
@@ -0,0 +1,35 @@
+// let a = document.getElementById("heading");
+// console.log(a);
+
+// let para = document.getElementsByTagName("p");
+// console.log(para);
+// console.dir(para);
+
+// let first = document.querySelector("p");
+// console.dir(first);
+
+// let a = document.querySelector("h3");
+// console.dir(a);
+
+// let ab = document.querySelector("h2");
+// console.dir(ab);
+
+// let box = document.querySelectorAll(".box");
+// console.log(box);
+
+// box[1].innerText = "new text from DOM";
+
+// let i = 1;
+// for(div of box) {
+//     div.innerText = `new box from DOM ${i}`;
+//     i++;
+// }
+
+// let ab = document.querySelector(".box");
+// console.log(ab);
+
+// let a = ab.getAttribute("class");
+// console.log(a);
+
+let para = document.querySelector("#myPara");
+console.log(para);
