256665a416a3ca41923dc6ff065ae10f8c66862e
nazmulh0001 <nazmulhasangub001@gmail.com>
2026-09-02 18:38:26 +0600

    question-3 solved

--- DOM/answer.js
+++ DOM/answer.js
@@ -18,40 +18,66 @@
 
 //Question 2
 
-function getDayType(value) {
-    switch (value) {
-        case "Friday":
-        case "friday":
-        case "FRIDAY":
-        case "Saturday":
-        case "saturday":
-        case "SATURDAY":
-            return "Weekend";
-            break;
-        case "Sunday":
-        case "SUNDAY":
-        case "sunday":
-        case "Monday":
-        case "monday":
-        case "MONDAY":
-        case "Tuesday":
-        case "tuesday" :
-        case "TUESDAY":
-        case "Wednesday":
-        case "wednesday":
-        case "WEDNESDAY":
-        case "Thursday":
-        case "THURSDAY":
-        case "thursday":
-            return "Working Day"; break;
-        default:
-            return "Invalid Day";
-            break;
-    }
-}
+// function getDayType(value) {
+//     switch (value) {
+//         case "Friday":
+//         case "friday":
+//         case "FRIDAY":
+//         case "Saturday":
+//         case "saturday":
+//         case "SATURDAY":
+//             return "Weekend";
+//             break;
+//         case "Sunday":
+//         case "SUNDAY":
+//         case "sunday":
+//         case "Monday":
+//         case "monday":
+//         case "MONDAY":
+//         case "Tuesday":
+//         case "tuesday" :
+//         case "TUESDAY":
+//         case "Wednesday":
+//         case "wednesday":
+//         case "WEDNESDAY":
+//         case "Thursday":
+//         case "THURSDAY":
+//         case "thursday":
+//             return "Working Day"; break;
+//         default:
+//             return "Invalid Day";
+//             break;
+//     }
+// }
 
-//test case for question 2
-console.log(getDayType("Friday"));
-console.log(getDayType("friday"));
-console.log(getDayType("MONDAY"));
-console.log(getDayType("Bandarban"));
+// //test case for question 2
+// console.log(getDayType("Friday"));
+// console.log(getDayType("friday"));
+// console.log(getDayType("MONDAY"));
+// console.log(getDayType("Bandarban"));
+
+
+//question 3
+
+// function validateUsername(userName) {
+//     if(userName.toLowerCase().length < 4) {
+//         return "Too Short";
+//     } else if(userName.toLowerCase().includes(" ")) {
+//         return "No Space Allowed";
+//     } else if(userName.toLowerCase().includes("admin")) {
+//         return "Reserved Word";
+//     } else {
+//         return "Available";
+//     }
+// }
+
+// //test cases for question 3
+// console.log(validateUsername("rahim123"));
+// console.log(validateUsername("ab"));
+// console.log(validateUsername("a b"));
+// console.log(validateUsername("abcd"));
+// console.log(validateUsername("rahim islam"));
+// console.log(validateUsername("superadmin99"));
+// console.log(validateUsername("Admin_Rahim"));
+
+
